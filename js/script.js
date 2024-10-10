2c5837f3a5b032716264c2778d212527fa55e892
zoehychan <zoecchan@outlook.com>
2024-10-10 10:52:58 +0800

    added progressive scrollbar and updated About content

--- js/script.js
+++ js/script.js
@@ -16,3 +16,10 @@ if (localStorage.getItem('theme') === 'dark') {
 html.classList.add('dark');
 }
 
+window.onscroll = function() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+  };
+
